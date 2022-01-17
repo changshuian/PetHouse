1932f139c6ed8559ec000d03723570c42b5c7509
changshuian <shuianlove@gmail.com>
2022-01-18 00:14:44 +0800

    add

--- ShoppingCart.js
+++ ShoppingCart.js
@@ -2,28 +2,37 @@ const input_reduce = document.querySelectorAll('.input_reduce');
 const input_value = document.querySelectorAll('.input_value');
 const input_add = document.querySelectorAll('.input_add');
 
-input_add[0].onclick = function() {
-    input_value[0].value++;
-    input_value[1].value++;
-}
-input_add[1].onclick = function() {
-    input_value[0].value++;
-    input_value[1].value++;
-}
-
+try {
+    input_add[0].onclick = function() {
+        input_value[0].value++;
+        input_value[1].value++;
+    }
+} catch {}
+try {
+    input_add[1].onclick = function() {
+        input_value[0].value++;
+        input_value[1].value++;
+    }
+} catch {}
 input_reduce[0].onclick = function() {
     if (input_value[0].value > 1) {
         input_value[0].value--;
     }
-    if (input_value[1].value > 1) {
-        input_value[1].value--;
-    }
+    try {
+        if (input_value[1].value > 1) {
+            input_value[1].value--;
+        }
+    } catch {}
 }
 input_reduce[1].onclick = function() {
-    if (input_value[0].value > 1) {
-        input_value[0].value--;
-    }
-    if (input_value[1].value > 1) {
-        input_value[1].value--;
-    }
+    try {
+        if (input_value[0].value > 1) {
+            input_value[0].value--;
+        }
+    } catch {}
+    try {
+        if (input_value[1].value > 1) {
+            input_value[1].value--;
+        }
+    } catch {}
 }
